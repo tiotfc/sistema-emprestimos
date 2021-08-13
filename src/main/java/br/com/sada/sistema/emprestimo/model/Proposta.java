4ad3d4471a9b4212663b062fbf705ae09ace9ddb
Henrique <henrique.tio@gmail.com>
2021-08-12 22:34:48 -0300

    modelagem e projeto

--- src/main/java/br/com/sada/sistema/emprestimo/model/Proposta.java
+++ src/main/java/br/com/sada/sistema/emprestimo/model/Proposta.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -28,7 +29,7 @@ public class Proposta {
 	@JsonIgnore
 	private Cliente cliente;
 
-	@OneToMany(mappedBy = "proposta")
+	@OneToMany(mappedBy = "proposta", cascade = CascadeType.ALL)
 	private List<Parcela> parcelas;
 
 	public Proposta() {
